91aa2bcd956bd3abb9a26531391703ab25d62412
cololaborde <cololaborde@gmail.com>
2022-03-04 15:19:26 -0300

    refacto and lint

--- file_cipher.py
+++ file_cipher.py
@@ -3,7 +3,6 @@
 import tkinter
 from tkinter import filedialog, messagebox
 import base64
-import re
 
 base = {'+':'<','/':'/',',':'!','.':'?',
         ' ':' ','a':'x','b':'y','c':'z',
@@ -59,42 +58,45 @@ def co_decode(script, level, code):
             line = content
     return content
 
-def code_binary(binary, code, filename):
-    """ code or decode binary file getting base64 encoding """
-    with open(filename.split('.')[0]+'.txt','wb') as f2:
-       coded = ""
-       while True:
+def code_binary(binary, level, filename):
+    """ code binary file getting base64 encoding """
+    with open(filename.split('.')[0]+'.txt','wb') as out:
+        coded = ""
+        while True:
             buf=binary.read()
             if buf:
                 b64_encode = base64.b64encode(buf)
-                coded = coded + co_decode(b64_encode.decode(), level=1, code=code)
+                coded = coded + co_decode(b64_encode.decode(), level=level, code=True)
             else:
-                f2.write(coded.encode())
-                f2.write(("."+filename.split(".")[len(filename.split("."))-1]).encode())
-                f2.close()
+                out.write(coded.encode())
+                out.write(("."+filename.split(".")[len(filename.split("."))-1]).encode())
+                out.close()
                 break
 
-def decode_binary(each, file_read, level, code, filename):
-    with open(each, 'r') as file:
+def decode_binary(each, file_read, level, filename):
+    """ decode binary file reading enconding from txt file """
+    with open(each, 'r', encoding='utf-8') as file:
         output_extension = file_read.split(".")[1]
         output_filename = filename.split(".")[0] + '.'+output_extension
         with open(output_filename, 'wb') as out:
             decoded = ""
             while True:
-                 buf=file.read()
-                 if buf:
+                buf=file.read()
+                print(buf == file_read)
+                if buf:
                     if len(str(buf).split(".")) > 1:
-                        buf = str(buf).split(".")[0]
-                    decoded = decoded + co_decode(buf, level=1, code=False)
-                 else:
+                        buf = str(file_read).split(".", maxsplit=1)[0]
+                    decoded = decoded + co_decode(buf, level=level, code=False)
+                else:
                     out.write(base64.b64decode(decoded.encode()))
                     out.close()
                     break
 
 def get_filename(each, yesno):
+    """return filename determining if file was coded or decoded"""
     ext = "."+each.split(".")[len(each.split("."))-1]
-    END = "-coded" if yesno else "-decoded"
-    return each.split(".")[0] + END + ext
+    end = "-coded" if yesno else "-decoded"
+    return each.split(".")[0] + end + ext
 
 
 parent = tkinter.Tk() # Create the object
@@ -108,13 +110,13 @@ file_names = filedialog.askopenfilenames(title='Select one or more files',
                                         filetypes=file_types, parent=parent)
 
 if len(file_names) > 0:
-    yesno = messagebox.askquestion(None, "Code (YES) or Decode (NO)?", icon ='question')
+    YESNO = messagebox.askyesno(None, "Code (YES) or Decode (NO)?", icon ='question')
     for each in file_names:
-        filename = get_filename(each, yesno)
-        if yesno:
+        filename = get_filename(each, YESNO)
+        if YESNO:
             with open(each, 'rb') as file:
-                result = code_binary(file, code=True, filename=filename)
+                code_binary(file, level=1, filename=filename)
         else:
             with open(each, 'r', encoding='utf8') as file:
                 file_read = file.read()
-                decode_binary(each, file_read=file_read, level=1, code=False, filename=filename)
+                decode_binary(each, file_read=file_read, level=1, filename=filename)
