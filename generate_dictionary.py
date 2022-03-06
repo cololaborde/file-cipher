45b8320b532416b385dbf09f675b56b0052afed1
cololaborde <cololaborde@gmail.com>
2022-03-06 18:40:33 -0300

    add generate random dict

---
+++ generate_dictionary.py
@@ -0,0 +1,41 @@
+import random 
+
+a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+     'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 
+     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 
+     'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E',
+     'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+     'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T',
+     'U', 'V', 'W', 'X', 'Y', 'Z', '¿', '?',
+     '=', '+', '-', '/', '>', '<', ',','[',
+     ']', ' ', '_', '!', '¡', ':', ';', '1',
+     '2', '3', '4', '5', '6', '7', '8', '9']
+
+base = {}
+yxpb = {}
+
+keys = a.copy()
+values = a.copy()
+
+
+while True:
+    index = random.randint(0, len(keys)-1)
+    key = keys[index]
+    while True:
+        index_value = random.randint(0, len(values)-1)
+        value = values[index_value]
+        if value != key:
+            keys.pop(index)
+            values.pop(index_value)
+            base[key] = value
+            yxpb[value] = key
+            break
+    if len(keys) == 0:
+        break
+    
+
+base['']=''
+yxpb['']=''
+print(base)
+print(yxpb)
+ 
