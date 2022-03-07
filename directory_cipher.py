c929359de53f493c5d421159641dd66f5b97a11f
cololaborde <cololaborde@gmail.com>
2022-03-07 13:19:29 -0300

    now iterate over subdirectories

--- directory_cipher.py
+++ directory_cipher.py
@@ -16,14 +16,15 @@ directory = filedialog.askdirectory(title='Select directory', parent=parent)
 
 if os.path.isdir(directory):
     YESNO = messagebox.askyesno(None, "Code (YES) or Decode (NO)?", icon ='question')
-    for each in os.listdir(directory):
-        filename = directory+'/'+each
-        if YESNO:
-            with open(filename, 'rb') as file:
-                file_read = file.read()
-                cipher.code_binary(readed_file=file_read, level=1, filepath=filename)
-        else:
-            with open(filename, 'r', encoding='utf8') as file:
-                file_read = file.read()
-                cipher.decode_binary(readed_file=file_read, level=1, filepath=filename)
-        os.remove(filename)
+    for root, subdirectories, files in os.walk(directory):
+        for file in files:
+            filename = os.path.join(root, file)
+            if YESNO:
+                with open(filename, 'rb') as file:
+                    file_read = file.read()
+                    cipher.code_binary(readed_file=file_read, level=1, filepath=filename)
+            else:
+                with open(filename, 'r', encoding='utf8') as file:
+                    file_read = file.read()
+                    cipher.decode_binary(readed_file=file_read, level=1, filepath=filename)
+            os.remove(filename)
