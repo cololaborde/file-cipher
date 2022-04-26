58d5bbd4d9a158272c4d79e7568046e3991359be
cololaborde <cololaborde@gmail.com>
2022-04-26 01:41:52 -0300

    simple refactor

--- desktop_app/aux_code/generate_dictionary.py
+++ desktop_app/aux_code/generate_dictionary.py
@@ -2,6 +2,7 @@
 
 import random
 
+
 chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
      'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o',
      'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
@@ -13,6 +14,16 @@ chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
      ']', ' ', '_', '!', '¡', ':', ';', '%',
      '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
+
+def generate_hash():
+    """ create random hash """
+    aux_dict = chars.copy()
+    passwd = ""
+    for _ in range(10):
+        passwd = passwd + aux_dict[random.randint(0, len(aux_dict) - 1)]
+    return passwd
+
+
 def create():
     """ generate and return codification/decodification dictionary """
     to_code = {}
@@ -20,20 +31,34 @@ def create():
     used_keys = []
     used_values = []
 
-    for _ in range(len(chars)):
-        char_key = chars[random.randint(0, len(chars)-1)]
+    def get_random_char():
+        return chars[random.randint(0, len(chars)-1)]
+
+    def get_char_key():
+        #get non duplicated char for keys
+        char_key = get_random_char()
         while char_key in used_keys:
-            char_key = chars[random.randint(0, len(chars)-1)]
-        used_keys.append(char_key)
-        char_value = chars[random.randint(0, len(chars)-1)]
-        
-        #para el ultimo caso
+            char_key = get_random_char()
+        return char_key
+
+    def get_char_value():
+        #get non duplicated char for values
+        char_value = get_random_char()
+        #for lastone entry
         if len(used_keys) == len(chars):
             while char_value in used_values:
                 char_value = chars[random.randint(0, len(chars)-1)]
         else:
             while char_key == char_value or char_value in used_values:
                 char_value = chars[random.randint(0, len(chars)-1)]
+        return char_value
+
+
+    for _ in range(len(chars)):
+        char_key = get_char_key()
+        used_keys.append(char_key)
+
+        char_value = get_char_value()
         used_values.append(char_value)
 
         to_code[char_key] = char_value
@@ -45,23 +70,19 @@ def create():
 
     return to_code, to_decode
 
-def generate_hash():
-    """ create random hash """
-    aux_dict = chars.copy()
-    passwd = ""
-    for _ in range(10):
-        passwd = passwd + aux_dict[random.randint(0, len(aux_dict) - 1)]
-    return passwd
 
+########  Main  ########
 
-dict_of_dicts = {}
+if __name__ == "__main__":
 
-for i in range(100):
-    aux = {}
-    base, yxpb = create()
-    aux['base'] = base
-    aux['yxpb'] = yxpb
-    dict_of_dicts[generate_hash()] = aux
+    dict_of_dicts = {}
 
-with open('mod.txt', 'w') as out:
-    out.write(str(dict_of_dicts))
+    for i in range(100):
+        aux = {}
+        base, yxpb = create()
+        aux['base'] = base
+        aux['yxpb'] = yxpb
+        dict_of_dicts[generate_hash()] = aux
+
+    with open('mod.txt', 'w', encoding='utf-8') as out:
+        out.write(str(dict_of_dicts))
